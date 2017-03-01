fc5b2be3fd6bb950199adbfaca859527029c6513
Mogens Heller Grabe <mookid8000@gmail.com>
2017-03-01 17:34:31 +0100

    native defer test

---
+++ Rebus.SqlServer.Tests/Bugs/TestNativeDeferToSomeoneElse.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Bus;
+using Rebus.Config;
+using Rebus.Messages;
+using Rebus.Pipeline;
+using Rebus.Routing;
+using Rebus.Routing.TypeBased;
+using Rebus.SqlServer.Transport;
+using Rebus.Tests.Contracts;
+using Rebus.Tests.Contracts.Extensions;
+
+#pragma warning disable 1998
+
+namespace Rebus.SqlServer.Tests.Bugs
+{
+    [TestFixture]
+    public class TestNativeDeferToSomeoneElse : FixtureBase
+    {
+        static readonly string ConnectionString = SqlTestHelper.ConnectionString;
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task ItWorks_SetDestinationHeaderFromTheOutside(bool usePipelineStep)
+        {
+            var receiver = new BuiltinHandlerActivator();
+
+            Using(receiver);
+
+            Configure.With(receiver)
+                .Transport(t => t.UseSqlServer(ConnectionString, "RebusMessages", "receiver"))
+                .Start();
+
+            var senderBus = Configure.With(new BuiltinHandlerActivator())
+                .Transport(x => x.UseSqlServerAsOneWayClient(ConnectionString, "RebusMessages"))
+                .Routing(r => r.TypeBased().Map<string>("receiver"))
+                .Options(o =>
+                {
+                    if (usePipelineStep)
+                    {
+                        o.Decorate<IPipeline>(c =>
+                        {
+                            var pipeline = c.Get<IPipeline>();
+                            var step = new AutoDeferredRecipientStep(c.Get<IRouter>());
+
+                            return new PipelineStepConcatenator(pipeline)
+                                .OnSend(step, PipelineAbsolutePosition.Front);
+                        });
+                    }
+                })
+                .Start();
+
+            Using(senderBus);
+
+            var gotTheString = new ManualResetEvent(false);
+
+            receiver.Handle<string>(async message => gotTheString.Set());
+
+            var optionalHeaders = usePipelineStep
+                ? new Dictionary<string, string>()
+                : new Dictionary<string, string> { { Headers.DeferredRecipient, "receiver" } };
+
+            await senderBus.Defer(TimeSpan.FromSeconds(1), "HEEELOOOOOO", optionalHeaders);
+
+            gotTheString.WaitOrDie(TimeSpan.FromSeconds(5));
+        }
+
+        class AutoDeferredRecipientStep : IOutgoingStep
+        {
+            readonly IRouter _router;
+
+            public AutoDeferredRecipientStep(IRouter router)
+            {
+                _router = router;
+            }
+
+            public async Task Process(OutgoingStepContext context, Func<Task> next)
+            {
+                var message = context.Load<Message>();
+
+                string temp;
+
+                if (message.Headers.TryGetValue(Headers.DeferredUntil, out temp))
+                {
+                    if (!message.Headers.TryGetValue(Headers.DeferredRecipient, out temp))
+                    {
+                        try
+                        {
+                            message.Headers[Headers.DeferredRecipient] = await _router.GetDestinationAddress(message);
+                        }
+                        catch (Exception exception)
+                        {
+                            throw new ApplicationException($"Could not automatically set recipient for deferred message", exception);
+                        }
+                    }
+                }
+
+                await next();
+            }
+        }
+    }
+}
--- Rebus.SqlServer.Tests/Integration/TestSqlAllTheWay.cs
+++ Rebus.SqlServer.Tests/Integration/TestSqlAllTheWay.cs
@@ -16,17 +16,20 @@ namespace Rebus.SqlServer.Tests.Integration
     [TestFixture, Category(Categories.SqlServer)]
     public class TestSqlAllTheWay : FixtureBase
     {
-        BuiltinHandlerActivator _handlerActivator;
-        IBus _bus;
         static readonly string ConnectionString = SqlTestHelper.ConnectionString;
+
+        BuiltinHandlerActivator _activator;
+        IBus _bus;
 
         protected override void SetUp()
         {
             DropTables();
 
-            _handlerActivator = new BuiltinHandlerActivator();
+            _activator = new BuiltinHandlerActivator();
 
-            _bus = Configure.With(_handlerActivator)
+            Using(_activator);
+
+            _bus = Configure.With(_activator)
                 .Transport(x => x.UseSqlServer(ConnectionString, "RebusMessages", "test.input"))
                 .Sagas(x => x.StoreInSqlServer(ConnectionString, "Sagas", "SagaIndex"))
                 .Options(x =>
@@ -35,8 +38,6 @@ namespace Rebus.SqlServer.Tests.Integration
                     x.SetMaxParallelism(1);
                 })
                 .Start();
-
-            Using(_bus);
         }
 
         protected override void TearDown()
@@ -57,7 +58,7 @@ namespace Rebus.SqlServer.Tests.Integration
             var gotTheMessage = new ManualResetEvent(false);
             var receivedMessageCount = 0;
 
-            _handlerActivator.Handle<string>(async message =>
+            _activator.Handle<string>(async message =>
             {
                 Interlocked.Increment(ref receivedMessageCount);
                 Console.WriteLine("w00000t! Got message: {0}", message);
