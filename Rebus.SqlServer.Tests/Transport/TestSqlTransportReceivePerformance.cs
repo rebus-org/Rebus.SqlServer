63ac9c64cdd3040a2632a856df8643484a0f33dc
Mogens Heller Grabe <mookid8000@gmail.com>
2019-09-10 09:49:56 +0200

    add test to verify SQL transport receive performance improvement

--- Rebus.SqlServer.Tests/Transport/TestSqlTransportReceivePerformance.cs
+++ Rebus.SqlServer.Tests/Transport/TestSqlTransportReceivePerformance.cs
@@ -8,6 +8,7 @@ using Rebus.Config;
 using Rebus.Logging;
 using Rebus.Tests.Contracts;
 using Rebus.Tests.Contracts.Utilities;
+// ReSharper disable ArgumentsStyleOther
 
 #pragma warning disable 1998
 
@@ -16,66 +17,80 @@ namespace Rebus.SqlServer.Tests.Transport
     [TestFixture, Category(Categories.SqlServer)]
     public class TestSqlTransportReceivePerformance : FixtureBase
     {
-        BuiltinHandlerActivator _adapter;
-
         const string QueueName = "perftest";
 
         static readonly string TableName = TestConfig.GetName("perftest");
 
-        static readonly string IndexCreationScriptToCheck = $@"
-CREATE INDEX IX_{TableName.ToUpperInvariant()}_ID
-    ON [dbo].[{TableName}] ([id]) 
-    INCLUDE ([priority])
-";
-
         protected override void SetUp()
         {
             SqlTestHelper.DropTable(TableName);
+        }
 
-            _adapter = Using(new BuiltinHandlerActivator());
+        /*
+         *
+Can be used as a simple indicator of the receive performance of the SQL transport.
 
-            Configure.With(_adapter)
+Before PR 55:
+    *** Using LEASE-BASED SQL transport ***
+    1000 messages received in 6,5 s - that's 152,7 msg/s
+
+    *** Using LEASE-BASED SQL transport ***
+    10000 messages received in 56,3 s - that's 177,7 msg/s
+
+After PR 55:
+    *** Using LEASE-BASED SQL transport ***
+    1000 messages received in 2,0 s - that's 499,4 msg/s
+
+    *** Using LEASE-BASED SQL transport ***
+    10000 messages received in 41,2 s - that's 242,7 msg/s
+         *
+         */
+        [TestCase(1000, true)]
+        [TestCase(1000, false)]
+        [TestCase(10000, true)]
+        [TestCase(10000, false)]
+        public async Task CheckReceivePerformance(int messageCount, bool useLeaseBasedTransport)
+        {
+            var adapter = Using(new BuiltinHandlerActivator());
+
+            Configure.With(adapter)
                 .Logging(l => l.ColoredConsole(LogLevel.Warn))
-                .Transport(t => t.UseSqlServer(SqlTestHelper.ConnectionString, QueueName))
+                .Transport(t =>
+                {
+                    if (useLeaseBasedTransport)
+                    {
+                        Console.WriteLine("*** Using LEASE-BASED SQL transport ***");
+                        t.UseSqlServerInLeaseMode(new SqlServerLeaseTransportOptions(SqlTestHelper.ConnectionString), QueueName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("*** Using NORMAL SQL transport ***");
+                        t.UseSqlServer(new SqlServerTransportOptions(SqlTestHelper.ConnectionString), QueueName);
+                    }
+                })
                 .Options(o =>
                 {
                     o.SetNumberOfWorkers(0);
                     o.SetMaxParallelism(20);
                 })
                 .Start();
-        }
-
-        [TestCase(1000, true, false)]
-        [TestCase(1000, true, true)]
-        [TestCase(1000, false, false)]
-        [TestCase(10000, true, false, Ignore = "run manually")]
-        [TestCase(10000, true, true, Ignore= "run manually")]
-        [TestCase(10000, false, false, Ignore= "run manually")]
-        public async Task NizzleName(int messageCount, bool useNewIndexScript, bool dropOldIndex)
-        {
-            if (useNewIndexScript)
-            {
-                SqlTestHelper.Execute(IndexCreationScriptToCheck);
-
-                SqlTestHelper.DropIndex(TableName, $"IDX_RECEIVE_{TableName}");
-            }
 
             Console.WriteLine($"Sending {messageCount} messages...");
 
             await Task.WhenAll(Enumerable.Range(0, messageCount)
-                .Select(i => _adapter.Bus.SendLocal($"THIS IS MESSAGE {i}")));
+                .Select(i => adapter.Bus.SendLocal($"THIS IS MESSAGE {i}")));
 
-            var counter = new SharedCounter(messageCount);
+            var counter = Using(new SharedCounter(messageCount));
 
-            _adapter.Handle<string>(async message => counter.Decrement());
+            adapter.Handle<string>(async message => counter.Decrement());
 
             Console.WriteLine("Waiting for messages to be received...");
 
             var stopwtach = Stopwatch.StartNew();
 
-            _adapter.Bus.Advanced.Workers.SetNumberOfWorkers(3);
+            adapter.Bus.Advanced.Workers.SetNumberOfWorkers(3);
 
-            counter.WaitForResetEvent(messageCount / 500 + 5);
+            counter.WaitForResetEvent(timeoutSeconds: messageCount / 100 + 5);
 
             var elapsedSeconds = stopwtach.Elapsed.TotalSeconds;
 
