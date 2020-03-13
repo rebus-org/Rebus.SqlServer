9ee3ca49d3ccee6ff5b1efe20e9c53e92c3d70c5
IvanovRS <ivanovr@psbank.ru>
2020-03-13 12:58:38 +0300

    Implement autoDeleteQueue option for SqlServer transport. It's useful for transient queues in Rebus.SignalR backplane (https://github.com/rebus-org/Rebus.SignalR/issues/3)

---
+++ Rebus.SqlServer.Tests/Transport/TestSqlServerTransportAutoDelete.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Logging;
+using Rebus.Tests.Contracts;
+
+namespace Rebus.SqlServer.Tests.Transport
+{
+	[TestFixture, Category(Categories.SqlServer)]
+	public class TestSqlServerTransportAutoDelete : FixtureBase
+    {
+        protected override void SetUp()
+        {
+            SqlTestHelper.DropAllTables();
+        }
+
+        [Test]
+        public async Task Dispose_WhenAutoDeleteQueueEnabled_DropsInputQueue()
+        {
+            var consoleLoggerFactory = new ConsoleLoggerFactory(false);
+            var connectionProvider = new DbConnectionProvider(SqlTestHelper.ConnectionString, consoleLoggerFactory);
+
+            const string queueName = "input";
+            
+            var options = new SqlServerTransportOptions(SqlTestHelper.ConnectionString);
+
+            Configure.With(new BuiltinHandlerActivator())
+                .Logging(l => l.Use(consoleLoggerFactory))
+                .Transport(t => t.UseSqlServer(options, queueName).SetAutoDeleteQueue(true));
+            
+            using (var connection = await connectionProvider.GetConnection())
+            {
+                Assert.False(connection.GetTableNames().Contains(TableName.Parse(queueName)));
+            }
+        }
+    }
+}
