da91cd81b51c1a4842f10326ce4e7b5252174a3a
Mogens Heller Grabe <mookid8000@gmail.com>
2020-09-13 18:48:31 +0200

    remove unused method signatures

--- Rebus.SqlServer/SqlServer/Sagas/Serialization/ISagaSerializer.cs
+++ Rebus.SqlServer/SqlServer/Sagas/Serialization/ISagaSerializer.cs
@@ -6,19 +6,9 @@
     public interface ISagaSerializer
     {
         /// <summary>
-        /// Serializes the given object into a byte[]
-        /// </summary>
-        byte[] Serialize(object obj);
-
-        /// <summary>
         /// Serializes the given object into a string
         /// </summary>
         string SerializeToString(object obj);
-
-        /// <summary>
-        /// Deserializes the given byte[] into an object
-        /// </summary>
-        object Deserialize(byte[] bytes);
 
         /// <summary>
         /// Deserializes the given string into an object
