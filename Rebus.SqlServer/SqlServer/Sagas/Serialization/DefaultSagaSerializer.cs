fe985e5a3e1fd829f4d37726571c7e459c15f312
mathiasnohall <mathias.nohall@gmail.com>
2020-09-13 16:04:14 +0200

    added comments for DefaultSagaSerializer

--- Rebus.SqlServer/SqlServer/Sagas/Serialization/DefaultSagaSerializer.cs
+++ Rebus.SqlServer/SqlServer/Sagas/Serialization/DefaultSagaSerializer.cs
@@ -2,6 +2,11 @@
 
 namespace Rebus.SqlServer.Sagas.Serialization
 {
+    /// <summary>
+    /// The default serializer for serializing sql saga data,
+    /// Implement <seealso cref="ISagaSerializer"/> to make your own custom serializer and register it using the UseSagaSerializer extension method.
+    /// <seealso cref="Rebus.Config.SqlServerSagaConfigurationExtensions.UseSagaSerializer"/>
+    /// </summary>
     public class DefaultSagaSerializer : ObjectSerializer, ISagaSerializer
     {
     }
