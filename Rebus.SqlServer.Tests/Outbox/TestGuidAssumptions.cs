92045c3281a876644a7bc4a212298b959c700cbc
Mogens Heller Grabe <mookid8000@gmail.com>
2021-10-12 09:08:14 +0200

    play with strategy for semi-unique ID generation

---
+++ Rebus.SqlServer.Tests/Outbox/TestGuidAssumptions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace Rebus.SqlServer.Tests.Outbox;
+
+[TestFixture]
+public class TestGuidAssumptions
+{
+    [TestCase(4)]
+    [TestCase(5)]
+    [TestCase(6)]
+    [TestCase(7)]
+    [TestCase(8)]
+    [Repeat(5)]
+    public void CheckGuidPrefix(int length) => RunTest(() => Guid.NewGuid().ToString("N").Substring(0, length));
+
+    [Test]
+    [Repeat(5)]
+    public void CheckTimestampHashCode() => RunTest(() => DateTime.Now.GetHashCode().ToString(CultureInfo.InvariantCulture));
+
+    private static void RunTest(Func<string> getNextId)
+    {
+        var ids = new HashSet<string>();
+
+        while (true)
+        {
+            var id = getNextId();
+
+            if (ids.Contains(id))
+            {
+                Console.WriteLine($"The ID {id} already exists - found {ids.Count} unique ids");
+                return;
+            }
+
+            ids.Add(id);
+
+            if (ids.Count == 10 * 1000 * 1000)
+            {
+                Console.WriteLine("OK that was 10M - quitting");
+                return;
+            }
+        }
+    }
+}
