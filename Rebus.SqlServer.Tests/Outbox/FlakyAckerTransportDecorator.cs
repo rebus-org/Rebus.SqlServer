21a1a31a42876d0ed021dbf9df2499e7ce44b600
Mogens Heller Grabe <mookid8000@gmail.com>
2022-05-05 14:12:03 +0200

    idempotency test

---
+++ Rebus.SqlServer.Tests/Outbox/FlakyAckerTransportDecorator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Rebus.Messages;
+using Rebus.Transport;
+// ReSharper disable AccessToModifiedClosure
+#pragma warning disable CS1998
+
+namespace Rebus.SqlServer.Tests.Outbox;
+
+class FlakyAckerTransportDecorator : ITransport
+{
+    readonly ITransport _transport;
+
+    int _ackFailures;
+
+    public FlakyAckerTransportDecorator(ITransport transport, int ackFailures)
+    {
+        _transport = transport ?? throw new ArgumentNullException(nameof(transport));
+        _ackFailures = ackFailures;
+    }
+
+    public void CreateQueue(string address) => _transport.CreateQueue(address);
+
+    public Task Send(string destinationAddress, TransportMessage message, ITransactionContext context) => _transport.Send(destinationAddress, message, context);
+
+    public async Task<TransportMessage> Receive(ITransactionContext context, CancellationToken cancellationToken)
+    {
+        TransportMessage transportMessage = null;
+
+        // be sure to register this completion handler first
+        async Task CompletedAction(ITransactionContext _)
+        {
+            // if no message was received, don't do anything
+            if (transportMessage == null) return;
+
+            // only at this point can we know whether we should throw
+            var throwException = Interlocked.Decrement(ref _ackFailures) + 1 >= 0;
+
+            // don't do anything 
+            if (!throwException) return;
+
+            Console.WriteLine("THROWING ACK FAILURE");
+
+            throw new AckFailureException("oh no could not ACK");
+        }
+
+        context.OnCompleted(CompletedAction);
+
+        transportMessage = await _transport.Receive(context, cancellationToken);
+
+        return transportMessage;
+    }
+
+    public string Address => _transport.Address;
+}
+
+class AckFailureException : Exception
+{
+    public AckFailureException(string message) : base(message)
+    {
+    }
+}
---
+++ Rebus.SqlServer.Tests/Outbox/TestOutboxIdempotency.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Tests.Contracts;
+using Rebus.Transport;
+using Rebus.Transport.InMem;
+// ReSharper disable AccessToDisposedClosure
+#pragma warning disable CS1998
+
+namespace Rebus.SqlServer.Tests.Outbox;
+
+[TestFixture]
+[Description("When everything clicks, except the final ACK of having received & processed a message, we will receive the same message again. We simulate that here.")]
+public class TestOutboxIdempotency : FixtureBase
+{
+    [Test]
+    public async Task DoesNotProcessSameMessageTwice()
+    {
+        using var countdown = new CountdownEvent(initialCount: 2);
+        using var adapter = new BuiltinHandlerActivator();
+
+        adapter.Handle<MyMessage>(async _ => countdown.Signal());
+
+        var bus = Configure.With(adapter)
+            .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "idempotency"))
+            .Options(o => o.Decorate<ITransport>(c => new FlakyAckerTransportDecorator(c.Get<ITransport>(), ackFailures: 1)))
+            .Start();
+
+        await bus.SendLocal(new MyMessage());
+
+        if (countdown.Wait(TimeSpan.FromSeconds(2)))
+        {
+            throw new AssertionException(
+                "Expected the countdown event to get a timeout, but it was decremented twice, which means that the message was handled twice!!!");
+        }
+    }
+
+    record MyMessage;
+}
