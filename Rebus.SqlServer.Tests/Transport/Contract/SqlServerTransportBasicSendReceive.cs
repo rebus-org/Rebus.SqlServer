9e9bd68787653e63c4a8a255c14f74564a0cf042
Mogens Heller Grabe <mookid8000@gmail.com>
2022-05-06 11:30:20 +0200

    fix tests

--- Rebus.SqlServer.Tests/Transport/Contract/SqlServerTransportBasicSendReceive.cs
+++ Rebus.SqlServer.Tests/Transport/Contract/SqlServerTransportBasicSendReceive.cs
@@ -5,4 +5,7 @@ using Rebus.Tests.Contracts.Transports;
 namespace Rebus.SqlServer.Tests.Transport.Contract;
 
 [TestFixture, Category(Categories.SqlServer)]
-public class SqlServerTransportBasicSendReceive : BasicSendReceive<SqlTransportFactory> { }
+public class SqlServerTransportBasicSendReceive : BasicSendReceive<SqlTransportFactory>
+{
+    protected override TransportBehavior Behavior => new(ReturnsNullWhenQueueIsEmpty: true);
+}
