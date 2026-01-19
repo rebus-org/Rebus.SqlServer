427ca6cc41669f6ecdad4468a5ac277f526d7a08
Mogens Heller Grabe <mookid8000@gmail.com>
2026-01-19 16:58:30 +0100

    try to reproduce bug

---
+++ Rebus.SqlServer.Tests/Bugs/ReplicateStartupError.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Serialization.Json;
+using Rebus.Tests.Contracts;
+
+namespace Rebus.SqlServer.Tests.Bugs;
+
+[TestFixture]
+[Description("Trying to reproduce an error reported by a used. Currently cannot reproduce.")]
+public class ReplicateStartupError : FixtureBase
+{
+    [Test]
+    public async Task CanDoIt()
+    {
+        var connectionString = SqlTestHelper.ConnectionString;
+
+        using var adapter = new BuiltinHandlerActivator();
+
+        using var bus = Configure.With(adapter)
+            .Logging(l => l.ColoredConsole())
+            .Transport(t => t.UseSqlServer(new SqlServerTransportOptions(connectionString), "messages"))
+            .Subscriptions(s => s.StoreInSqlServer(connectionString, "consumers", automaticallyCreateTables: true))
+            .Serialization(s => s.UseSystemTextJson())
+            .Start();
+
+        await Task.Delay(millisecondsDelay: 200);
+    }
+}
